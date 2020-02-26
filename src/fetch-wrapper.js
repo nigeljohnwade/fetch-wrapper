39e44d82409617cb619b0ad5bd875771e4016d63
Nigel Wade <nwade@shira.iouk.ioroot.tld>
2020-02-26 12:17:22 +0000

    Tidy up after switch to babel

--- src/fetch-wrapper.js
+++ src/fetch-wrapper.js
@@ -1,9 +1,16 @@
-export const fetchGetFromAddress = (url, options) => {
+export const fetchGetFromAddress = (url = '', options = {}) => {
+    if (!url || url === '') {
+        const error = new Error('No URL specified');
+        throw error;
+    }
     options.method = 'GET';
     fetch(url, options)
         .then((response) => {
             if(response.ok){
                 return response.json();
+            } else {
+                const error = new Error(`Response not OK: ${reponse.status}`);
+                throw error;
             }
         })
         .catch((error) => {
